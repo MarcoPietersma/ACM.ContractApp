b21163f517d4809ab05cfcd6436a29a2bf290ac6
Marco Pietersma <Marco.Pietersma@macaw.nl>
2025-12-02 15:37:00 +0100

    Add project files.

---
+++ ACM.ContractApp/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Contracts;
+
+namespace ACM.ContractApp;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Contract> Contracts => Set<Contract>();
+    
+}
---
+++ ACM.ContractApp/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ACM.ContractApp;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
+
+    public ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+}
+
+public class Contract
+{
+    public Guid Id { get; set; }
+
+    public Guid CustomerId { get; set; }
+
+    public Customer? Customer { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public decimal MonthlyAmount { get; set; }
+    
+}
